35cb27c1c067c21b34c5e4f7754b55a552891e61
kaipaynter <admin@beartigerproductions.com>
2023-05-02 08:02:06 +0000

    latest changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,21 +9,16 @@ const AboutPage = () => (
         <h2>About</h2>
       </header>
       <p className='center'>
-        Founded by Kai Paynter in January 2023, The Americas is an American-led (Canada, US, & South America) theatre company, operating out of Sydney, Australia.
-        The Americas hold the distinction of being the only American professional theatre company, (crew, talent, and works) in Australia.
-        Under the leadership of Producing Artistic Director, Kirk Carrington-Hastings and Managing Director, Kai Paynter, The America's commitment is to bring diverse, high-standard, American shows to the beautiful Syndey stages.
+        Founded in 2023, The Americas is an American (led) theatre company, operating out of Sydney, Australia. The Americas hold the distinction of being the only American professional theatre company, (crew, talent, and works) in Australia. Under the leadership of Producing Artistic Director, Kirk Carrington-Hastings and Managing Director, Kai Paynter, The America's commitment is to bring diverse, high-standard, American shows to the beautiful Syndey stages.
       </p>
       <p className='center'>
         <h4>Mission</h4>
-
-        The Americas Theatre Company creates exceptional theatrical experiences by using the arts to articulate messages that inspire freedom and hope - the cornerstones of American culture.
+        To be the premier producer of transformative American theatre in Australia that enhances the American cultural identity by telling authentic and multifaceted stories of the U.S. experience. The Americas create exceptional theatrical experiences by using the arts to articulate messages that inspire freedom & hope - the cornerstones of American culture.
       </p>
       <p className='center'>
         <h4>Values</h4>
-
-        The Americas Theatre Company believes the arts are essential because they foster the hero within, create a greater understanding of cultural and political differences and continue to provide a forum in which the larger community can gather live, in-person, and even online. We value theatre as a place where people can be accepted regardless of race, creed, orientation, or color.
+        The Americas Theatre Company believes the arts are essential and create a greater understanding of cultural and political differences. We value theatre as a place where people can be accepted regardless of race, creed, orientation, or color.
       </p>
-
     </article>
   </Layout>
 );
--- src/pages/submissions.js
+++ src/pages/submissions.js
@@ -10,19 +10,19 @@ const SubmissionsPage = () => (
         <h2>Submissions Policy</h2>
       </header>
       <p className='center'>
-        This coming season, we are thrilled to share a collection of truly remarkable works that shine a light on our shared humanity. We hope you join us on this artistic journey by submitting an expression of interest.
+        "Buckle up: it is going to be a wild ride!" - Robert Harrell, Actor
       </p>
       <p className='center'>
-        "Buckle up: it is going to be a wild ride!" - Robert Harrell, Actor
+        We are thrilled to share a collection of truly remarkable works that shine a light on our shared humanity. We hope you join us on this artistic journey by submitting an expression of interest.
       </p>
       <p className='center'>
         The Americas produce a wide range of theatre, from musicals/musical show cases to comedies, dramas, and original new work.
       </p>
       <p className='center'>
-        After initial review, candidates will be selected both for the promise shown around their individual artistic voice and practice, as well as for how that practice might contribute to collaborative creation within the values and mission of The America's Theatre Company. These candidates will be selected for individual contracts to one or more specific productions within our producing season. Australian actors/crew are encouraged to apply if they have a vested interest in American works, culture, history, etc.
+        After initial review, candidates will be selected both for the promise shown around their individual artistic voice and practice, as well as for how that practice might contribute to collaborative creation within the values and mission of The America's. These candidates will be selected for individual contracts to one or more specific productions within our producing season. Australian actors/crew are encouraged to apply if they have a vested interest in American works, culture, history, etc.
       </p>
       <p className='center'>
-        Candidates should email their professional headshot/resumes, portfolios (links or PDF), and a creative statement to contact@beartigerproductions.com.
+        In the first instance, candidates should email their professional headshot/resumes, portfolios (links or PDF), and a creative statement to contact@beartigerproductions.com
       </p>
       <p className='center'>
         All artists will be considered regardless of race, color, creed, religion, national origin, ancestry, age, sex, sexual orientation, gender identity and/or expression, political persuasion or belief, marital status, familial status, physical or mental disability, veteran status, or status as a whistleblower.
