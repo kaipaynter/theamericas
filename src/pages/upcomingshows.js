1376576406c5e24afb9087eec3d26fc3724a58bd
kaipaynter <admin@beartigerproductions.com>
2024-07-29 11:37:32 +0000

    missed changes

---
+++ src/pages/upcomingshows.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image'
+import Layout from '../components/Layout';
+
+const UpcomingShowsPage = () => (
+  <Layout fullMenu>
+    <article id="main">
+      <header />
+      <span className="image fit">
+        <p className='align-center'>
+          <a
+            href='/virtualprogram'
+          >	Click here for our virtual program
+          </a>
+        </p>
+        <StaticImage
+          src="../assets/images/L-O-V-E_Gala_Press_release.png"
+          alt="L-O-V-E Gala Press Release 1"
+          placeholder="blurred"
+          objectFit="contain"
+        />
+      </span>
+    </article>
+  </Layout>
+);
+
+export default UpcomingShowsPage;
