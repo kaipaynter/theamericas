4f9432ece026359ad08a89cbd2055773235f44c2
kaipaynter <admin@beartigerproductions.com>
2025-01-03 16:29:02 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2008fea988a4cd06a52f.js"
+    "url": "webpack-runtime-8212e3d491d6518d1ece.js"
   },
   {
     "url": "framework-2dfc5d12fe92a7fc4ecb.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b1db7ff95322d7ac2f0fcf04703abff5"
+    "revision": "764532030d5210e52677e577501117ed"
   },
   {
     "url": "manifest.webmanifest",
