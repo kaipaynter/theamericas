1a984e1d37b5d04eae32fd031113caab4fb98056
kaipaynter <admin@beartigerproductions.com>
2025-01-03 16:28:20 +0000

    modified:   src/pages/about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,10 +8,11 @@ const AboutPage = () => (
       <header>
       </header>
       <p className='center'>
+        <h4>Mission</h4>
         Founded in 2023, The Americas is an American-led theatre company, operating out of Sydney, Australia. It holds the distinction of being the only American-led theatre company in Australia. The Americas commitment is to bring diverse, high-standard shows to the beautiful Australian stages.
       </p>
       <p className='center'>
-        <h4>Mission</h4>
+        <h4>Vision</h4>
         To be the premier producer of transformative American theatre in Australia by telling authentic and multifaceted stories of the U.S. experience. The Americas also creates exceptional theatrical experiences by providing an individualistic and intensive growth approach for all artists - cast & crew, alike - honoring a commitment to excellence.
       </p>
       <p className='center'>
