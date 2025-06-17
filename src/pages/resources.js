deeeb246e6eb11de51aa838015b294c8e489b69f
kaipaynter <77270599+kaipaynter@users.noreply.github.com>
2025-06-17 08:25:20 +0200

    add new poster

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import loveprogram2025 from "../../static/L-O-V-E_Program_2025.pdf"
 import loveprogram2024 from "../../static/L-O-V-E_Program_2024.pdf"
+import posterqr from "../../static/Poster_QR.pdf"
 
 const ResourcesPage = () => (
     <Layout fullMenu>
@@ -19,6 +20,13 @@ const ResourcesPage = () => (
                     href={loveprogram2024}
                     target="\_blank"
                 >	L-O-V-E by Kai Paynter & Allan Staples 2024
+                </a>
+            </p>
+             <p className='align-center'>
+                <a
+                    href={posterqr}
+                    target="\_blank"
+                >	Amongst The Living
                 </a>
             </p>
         </article>
