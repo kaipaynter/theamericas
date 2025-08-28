4b8ce8e0fc04ef7756d37a647522e678de349b1c
kaipaynter <77270599+kaipaynter@users.noreply.github.com>
2025-08-28 18:18:15 +0200

    Update upcomingshows.js

--- src/pages/upcomingshows.js
+++ src/pages/upcomingshows.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 import atwtsily from "../assets/images/atwtsily_synopsis.jpg"
 import bis from "../assets/images/bis_synopsis.jpg"
 import jfitcdl from "../assets/images/jfitcdl_synopsis.jpg"
-import atwtsilyPoster from "../assets/images/ATWTSILY_poster_ALT.jpg"
+import atwtsilyPoster from "../assets/images/ABIS_poster.jpg"
 import bisPoster from "../assets/images/bis.jpg"
 import jfitcdlPoster from "../assets/images/jfitcdl.jpg"
 import titlecard from "../assets/images/new_show.png"
@@ -16,7 +16,7 @@ const shows = [
     alt: "Title Card for Upcoming Shows",
   },
   {
-    href: "https://beartigerprodmgmt.getform.com/p5l47",
+    href: "https://beartigerprodmgmt.getform.com/dxjpk",
     imgSrc: atwtsilyPoster,
     alt: "All the ways to say I love you",
   },
